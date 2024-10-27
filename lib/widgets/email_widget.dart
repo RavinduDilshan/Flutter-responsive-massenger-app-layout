59a681adb334217b4f3426b9a05dd50bf029b877
Ravidu Dilshan <36620171+RavinduDilshan@users.noreply.github.com>
2024-10-27 21:16:48 +0530

    email headling widget added

--- lib/widgets/email_widget.dart
+++ lib/widgets/email_widget.dart
@@ -109,17 +109,163 @@ class _EmailContentState extends State<EmailContent> {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: EdgeInsets.all(
+      padding: const EdgeInsets.all(
         20,
       ),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           LayoutBuilder(builder: (context, constraints) {
-            return Row();
-          })
+            return Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                if (constraints.maxWidth - 200 > 0) ...[
+                  CircleAvatar(
+                    backgroundImage: AssetImage(widget.email.sender.avatarUrl),
+                  ),
+                  const Padding(padding: EdgeInsets.symmetric(horizontal: 6))
+                ],
+                Expanded(
+                    child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      widget.email.sender.name.fullName,
+                      overflow: TextOverflow.fade,
+                      maxLines: 1,
+                      style: widget.isSelected
+                          ? _textTheme.labelMedium?.copyWith(
+                              color: _colorScheme.onSecondaryContainer)
+                          : _textTheme.labelMedium
+                              ?.copyWith(color: _colorScheme.onSurfaceVariant),
+                    )
+                  ],
+                )),
+                if (constraints.maxWidth - 200 > 0) ...[const StarButton()]
+              ],
+            );
+          }),
+          const SizedBox(width: 8),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              if (widget.isPreview) ...[
+                Text(
+                  widget.email.subject,
+                  style: const TextStyle(fontSize: 18)
+                      .copyWith(color: _colorScheme.onSurface),
+                )
+              ],
+              if (widget.isThreaded) ...[
+                contentSpacer,
+                Text(
+                    'To ${widget.email.recipients.map((recipient) => recipient.name.first).join(', ')}',
+                    style: _textTheme.bodyMedium),
+              ],
+              contentSpacer,
+              Text(
+                widget.email.content,
+                maxLines: widget.isPreview ? 2 : 100,
+                overflow: TextOverflow.ellipsis,
+                style: contentTextStyle,
+              )
+            ],
+          ),
+          const SizedBox(width: 12),
+          widget.email.attachments.isNotEmpty
+              ? Container(
+                  height: 96,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(8.0),
+                      image: DecorationImage(
+                          fit: BoxFit.cover,
+                          image:
+                              AssetImage(widget.email.attachments.first.url))),
+                )
+              : const SizedBox.shrink(),
+          if (!widget.isPreview) ...[const EmailReplyOptions()]
         ],
       ),
     );
+  }
+}
+
+class EmailHeadline extends StatefulWidget {
+  final Email email;
+  final bool isSelected;
+  const EmailHeadline(
+      {required this.email, required this.isSelected, super.key});
+
+  @override
+  State<EmailHeadline> createState() => _EmailHeadlineState();
+}
+
+class _EmailHeadlineState extends State<EmailHeadline> {
+  late final TextTheme _textTheme = Theme.of(context).textTheme;
+  late final ColorScheme _colorScheme = Theme.of(context).colorScheme;
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(builder: (context, constraints) {
+      return Container(
+        height: 84,
+        color: Color.alphaBlend(
+            _colorScheme.primary.withOpacity(0.05), _colorScheme.surface),
+        child: Padding(
+          padding: const EdgeInsets.fromLTRB(24, 12, 12, 12),
+          child: Row(
+            mainAxisSize: MainAxisSize.max,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Expanded(
+                  child: Column(
+                mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    widget.email.subject,
+                    maxLines: 1,
+                    overflow: TextOverflow.fade,
+                    style: const TextStyle(
+                        fontSize: 18, fontWeight: FontWeight.w400),
+                  ),
+                  Text(
+                    '${widget.email.replies.toString()} Messages',
+                    maxLines: 1,
+                    overflow: TextOverflow.fade,
+                    style: _textTheme.labelMedium
+                        ?.copyWith(fontWeight: FontWeight.w500),
+                  )
+                ],
+              )),
+              // Display a "condensed" version if the widget in the row are
+              // expected to overflow.
+              if (constraints.maxWidth - 200 > 0) ...[
+                SizedBox(
+                  height: 40,
+                  width: 40,
+                  child: FloatingActionButton(
+                    onPressed: () {},
+                    elevation: 0,
+                    backgroundColor: _colorScheme.surface,
+                    child: const Icon(Icons.delete_outline),
+                  ),
+                ),
+                const Padding(padding: EdgeInsets.only(right: 8)),
+                SizedBox(
+                  height: 40,
+                  width: 40,
+                  child: FloatingActionButton(
+                    onPressed: () {},
+                    elevation: 0,
+                    backgroundColor: _colorScheme.surface,
+                    child: const Icon(Icons.more_vert),
+                  ),
+                )
+              ]
+            ],
+          ),
+        ),
+      );
+    });
   }
 }
